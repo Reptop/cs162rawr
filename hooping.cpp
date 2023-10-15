6d402459f2df20b18dd0ec5e21cf0e2b14111b9a
Ren <36342803+Reptop@users.noreply.github.com>
2023-10-15 03:18:33 -0700

    Add files via upload

--- hooping.cpp
+++ hooping.cpp
@@ -21,6 +21,7 @@ bool success() {
     return true;
 }
 
+// ouputs error message, prompting user to try again
 void errorMsg() {
   cout << "Invalid input for either input." << endl;
   cout << "Both inputs must be between 1 and 5 (inclusive)" << endl;
@@ -47,13 +48,14 @@ bool playAgain() {
 
   while (running) {
     cout << "Do you want to play again? Enter (Y/N)" << endl;
+    cout << ">> ";
     cin >> n;
     if (n == 'N' || n == 'n') {
       cout << "Thanks for playing!" << endl;
       return false;
-    } else if (n == 'Y' || n == 'y') {
+    } else if (n == 'Y' || n == 'y')
       break;
-    } else {
+    else {
       cout << "Invalid input. Try again.\n" << endl;
       clearBuffer();
     }
@@ -64,11 +66,10 @@ bool playAgain() {
 // seperate function to only compute for the moneyBallRack
 void computeMoneyBoard(char arr[MAX_SIZE][MAX_SIZE], int moneyBallRack) {
   for (int i = 0; i < MAX_SIZE; ++i) {
-    if (success()) {
+    if (success())
       arr[moneyBallRack][i] = 'M';
-    } else {
+    else
       arr[moneyBallRack][i] = 'X';
-    }
   }
 }
 
@@ -78,22 +79,21 @@ void computeBoard(char arr[MAX_SIZE][MAX_SIZE], int moneyBallRack) {
     for (int j = 0; j < MAX_SIZE; ++j) {
       // if the loop reaches the last element, check for moneyball success
       if (j == MAX_SIZE - 1) {
-        if (success()) {
+        if (success())
           arr[i][j] = 'M';
-        } else {
+        else
           arr[i][j] = 'X';
-        }
-      } else if (success()) {
+      } else if (success())
         arr[i][j] = 'O';
-      } else {
+      else
         arr[i][j] = 'X';
-      }
     }
   }
-  // overwriting with moneyboard logic
+  // overwriting with moneyboard logic; found this implementation very easy
   computeMoneyBoard(arr, moneyBallRack - 1);
 }
 
+// function for starry ball logic; returns 3 or 0 for total score calculation
 int starryBalls() {
   if (success()) {
     cout << "Starry: " << 'W' << "          | 3 pts" << endl;
@@ -104,58 +104,28 @@ int starryBalls() {
   }
 }
 
-// prints elements within each rack, excluding starryballs
-void printFirstHalf(char arr[MAX_SIZE][MAX_SIZE], int &totalScore) {
-  int rack_number = 1;
-  int counter = 0;
-  for (int i = 0; i < 2; ++i) {
-    cout << "Rack " << rack_number << ": ";
-    ++rack_number;
-    for (int j = 0; j < MAX_SIZE; ++j) {
-      cout << arr[i][j] << " ";
-      if (arr[i][j] == 'O')
-        counter += 1;
-      else if (arr[i][j] == 'M') {
-        counter += 2;
-      }
-    }
-    cout << " | " << counter << " pts";
-    counter = 0;
-    cout << endl;
-  }
-  totalScore += starryBalls();
-}
+// prints pieces of the board, while also outputting scores for each rack
+void printBoard(char arr[MAX_SIZE][MAX_SIZE], int &totalScore, int start,
+                int limit, int rack_number) {
 
-void printSecondHalf(char arr[MAX_SIZE][MAX_SIZE], int &totalScore) {
-  int rack_number = 3;
   int counter = 0;
-  for (int i = 2; i < 4; ++i) {
+
+  for (int i = start; i < limit; ++i) {
     cout << "Rack " << rack_number << ": ";
     ++rack_number;
     for (int j = 0; j < MAX_SIZE; ++j) {
       cout << arr[i][j] << " ";
       if (arr[i][j] == 'O')
         counter += 1;
-      else if (arr[i][j] == 'M') {
+      else if (arr[i][j] == 'M')
         counter += 2;
-      }
     }
     cout << " | " << counter << " pts";
     counter = 0;
     cout << endl;
   }
-  totalScore += starryBalls();
-  cout << "Rack " << rack_number << ": ";
-  for (int i = 0; i < MAX_SIZE; ++i) {
-    cout << arr[MAX_SIZE - 1][i] << " ";
-    if (arr[MAX_SIZE - 1][i] == 'O')
-      counter += 1;
-    else if (arr[MAX_SIZE - 1][i] == 'M') {
-      counter += 2;
-    }
-  }
-  cout << " | " << counter << " pts";
-  cout << endl;
+  if (start == 0 || start == 2)
+    totalScore += starryBalls();
 }
 
 // scrubs input for only valid numbers - takes first instance of valid number
@@ -168,6 +138,14 @@ int inputScrubbing(string n) {
     }
   }
   return -1;
+}
+
+// to modulate my printBoard implementation; there is a better way of doing this
+// I'm just not smart enough to figure that out
+void printAll(char arr[MAX_SIZE][MAX_SIZE], int &pt) {
+  printBoard(arr, pt, 0, 2, 1);
+  printBoard(arr, pt, 2, 3, 3);
+  printBoard(arr, pt, 3, 5, 4);
 }
 
 // Performs one turn for the player, including all background computation
@@ -186,13 +164,13 @@ void playTurn(char arr[MAX_SIZE][MAX_SIZE], int p, int moneyBallRack, int &pt) {
         pt += 0;
     }
   }
-  printFirstHalf(arr, pt);
-  printSecondHalf(arr, pt);
+  printAll(arr, pt);
 
   cout << "\n";
   cout << "Total Score: " << pt << endl;
 }
 
+// this function takes user input and assigns valid input to scrubbedInput
 void getInputs(string input1, string input2, int &scrubbed1, int &scrubbed2) {
   cout << "Player 1, Where do you want to put your money ball rack?" << endl;
   cout << "Enter 1-5: ";
@@ -226,9 +204,9 @@ int main() {
   while (running) {
     getInputs(input1, input2, scrubbedInput1, scrubbedInput2);
 
-    if (scrubbedInput1 == -1 || scrubbedInput2 == -1) {
+    if (scrubbedInput1 == -1 || scrubbedInput2 == -1)
       errorMsg();
-    } else {
+    else {
       playTurn(rack1, 1, scrubbedInput1, playerTotal1);
       playTurn(rack1, 2, scrubbedInput2, playerTotal2);
       checkWin(playerTotal1, playerTotal2);
