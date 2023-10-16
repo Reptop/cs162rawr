be932e076c9a9875d24c83c0fd7d9aba9e43711f
Ren <36342803+Reptop@users.noreply.github.com>
2023-10-16 11:05:26 -0700

    Update dyna.cpp
    added actual error checking, fuck me

--- dyna.cpp
+++ dyna.cpp
@@ -2,17 +2,23 @@
 ** Program: hooping.cpp
 ** Author: Raed Kabir
 ** Date: 10/5/2023
-** Description: This program is a basketball shooting game simulator
-** Input: User inputs total number of players and their money ball rack
+** Description: This program is a basketball shooting game simulator.
+** Input: User inputs total number of desired players and their money ball rack.
 ** Output: This program outputs an entire score board with specific notation for
-*each ball
+*each ball, and the total scores for each player and rack
 ******************************************************/
 
 #include <iostream>
 using namespace std;
 #define MAX_SIZE 5
 
-// returns true if ball successfully scored; otherwise, return false
+/*
+ *Function: sucesss
+ * Description: returns true or false depending on a 50/50 chance
+ * Parameters: N/A
+ * Pre conditions: used in the context of computing shot sucesss
+ * Post Conditions: determines if array consits of specific notation
+ */
 bool success() {
   // implementing 50% chance
   if (rand() % 2 == 0)
@@ -21,7 +27,13 @@ bool success() {
     return true;
 }
 
-// free's the entire 3D array, and the playerScore array
+/*
+ *Function: endGame
+ * Description: Free's the entire 3D array, and the playerScore array
+ * Parameters: triple pointer char array, pointer int array, int for players
+ * Pre conditions: Arrays must be initilized to correct types
+ * Post Conditions: Arrays must be deleted from heap
+ */
 void endGame(char ***racks, int *playerScores, int players) {
   // free all memory inside out, even more so given the 3D array
   for (int p = 0; p < players; p++) {
@@ -34,17 +46,37 @@ void endGame(char ***racks, int *playerScores, int players) {
   delete[] playerScores;
 }
 
-// ouputs error message, prompting user to try again
+/*
+ *Function: errorMsg
+ * Description: outputs error message, prompting user to try again
+ * Parameters: N/A
+ * Pre conditions: Arrays must be initilized to correct types
+ * Post Conditions: Prompts the user to try again
+ */
 void errorMsg() {
   cout << "Invalid input." << endl;
   cout << "Inputs must be between 1 and 5 (inclusive)" << endl;
 }
 
+/*
+ *Function: clearBuffer
+ * Description: Clears input stream
+ * Parameters: N/A
+ * Pre conditions: Should have buffer present in input stream
+ * Post Conditions: Input stream is cleared
+ */
 void clearBuffer() {
   cin.clear();
   cin.ignore(1000000, '\n');
 }
 
+/*
+ *Function: checkWin
+ * Description: Calculates the winner of game
+ * Parameters: Pointer array for all scores, int for number of players
+ * Pre conditions: Both arrays should be populated
+ * Post Conditions: Outputs the winner of game
+ */
 void checkWin(int *playerScores, int players) {
   // set default maxScore to the first element
   int maxScore = playerScores[0];
@@ -70,7 +102,13 @@ void checkWin(int *playerScores, int players) {
   }
 }
 
-// return false if user does not want to play again; otherwise, returns true
+/*
+ *Function: playAgain
+ * Description: Return false/true if user does not want to play again
+ * Parameters: N/A
+ * Pre conditions: Game must have ended
+ * Post Conditions: Stops game loop or continues
+ */
 bool playAgain() {
   char n;
   bool running = true;
@@ -92,7 +130,13 @@ bool playAgain() {
   return true;
 }
 
-// seperate function to only compute for the moneyBallRack
+/*
+ *Function: computeMoneyBoard
+ * Description: Seperate function to only compute for the moneyBallRack
+ * Parameters: Single pointer char array, integer for moneyBallRack id
+ * Pre conditions: Game board must have been fully computed
+ * Post Conditions: gameBoard is modified with moneyboard logic
+ */
 void computeMoneyBoard(char *arr[MAX_SIZE], int moneyBallRack) {
   for (int i = 0; i < MAX_SIZE; ++i) {
     if (success())
@@ -102,7 +146,13 @@ void computeMoneyBoard(char *arr[MAX_SIZE], int moneyBallRack) {
   }
 }
 
-// computes logic for all racks; sets rack elements to X, O, or M
+/*
+ *Function: computeBoard
+ * Description: Computes logic for all racks; sets rack elements to X, O, or M
+ * Parameters: single pointer char array, moneyBallRack id
+ * Pre conditions: single pointer array must be initilized
+ * Post Conditions: Board will be populated with specific game notation
+ */
 void computeBoard(char *arr[MAX_SIZE], int moneyBallRack) {
   for (int i = 0; i < MAX_SIZE; ++i) {
     for (int j = 0; j < MAX_SIZE; ++j) {
@@ -122,7 +172,13 @@ void computeBoard(char *arr[MAX_SIZE], int moneyBallRack) {
   computeMoneyBoard(arr, moneyBallRack - 1);
 }
 
-// function for starry ball logic; returns 3 or 0 for total score calculation
+/*
+ *Function: starryBalls
+ * Description: Function for starry ball logic; returns 3 or 0
+ * Parameters: N/A
+ * Pre-conditions: board must be fully calculated
+ * Post Conditions: Board will be populated with starrry balls
+ */
 int starryBalls() {
   if (success()) {
     cout << "Starry: " << 'W' << "          | 3 pts" << endl;
@@ -133,7 +189,13 @@ int starryBalls() {
   }
 }
 
-// prints pieces of the board, while also outputting scores for each rack
+/*
+ *Function: printBoard
+ * Description: Prints pieces of the board
+ * Parameters: single pointer char array, int for total, start, limit, rac
+ * Pre-conditions: board must be fully calculated
+ * Post Conditions: Board will be outputted into terminal
+ */
 void printBoard(char *arr[MAX_SIZE], int &totalScore, int start, int limit,
                 int rack_number) {
 
@@ -157,7 +219,13 @@ void printBoard(char *arr[MAX_SIZE], int &totalScore, int start, int limit,
     totalScore += starryBalls();
 }
 
-// scrubs input for only valid numbers - takes first instance of valid number
+/*
+ *Function: inputScrubbing
+ * Description: Scrubs input for only valid numbers (1-5)
+ * Parameters: A string n
+ * Pre-conditions: A user must have inputted something into input stream
+ * Post Conditions: function parses through input and outputs and int
+ */
 int inputScrubbing(string n) {
   for (int i = 0; i < n.length(); ++i) {
     // detecting a char between 1 and 5
@@ -169,15 +237,26 @@ int inputScrubbing(string n) {
   return -1;
 }
 
-// to modulate my printBoard implementation; there is a better way of doing this
-// I'm just not smart enough to figure that out
+/*
+ *Function: printAll
+ * Description: To modulate my printBoard implementation; im not smart
+ * Parameters: single pointer char array, int for total Score
+ * Pre-conditions: Board must have fully been calculated
+ * Post Conditions: Prints the entire board fully into terminal
+ */
 void printAll(char *arr[MAX_SIZE], int &pt) {
   printBoard(arr, pt, 0, 2, 1);
   printBoard(arr, pt, 2, 3, 3);
   printBoard(arr, pt, 3, 5, 4);
 }
 
-// Performs one turn for the player, including all background computation
+/*
+ *Function: playTurn
+ * Description: Performs one turn for the player, including all background
+ * Parameters: single pointer char array, int for player, moneyBallRack, score
+ * Pre-conditions: User inputted their choice of moneyBallRack
+ * Post Conditions: Computes a single turn for one player; outputs their board
+ */
 void playTurn(char *arr[MAX_SIZE], int p, int moneyBallRack, int &pt) {
   pt = 0;
 
@@ -199,6 +278,13 @@ void playTurn(char *arr[MAX_SIZE], int p, int moneyBallRack, int &pt) {
   cout << "Total Score: " << pt << endl;
 }
 
+/*
+ *Function: getInputs
+ * Description: Takes in user input
+ * Parameters: int for scrubbedinput, and for the player number
+ * Pre-conditions: User must have inputted some number
+ * Post Conditions: Assigns a number to scrubbedInput
+ */
 void getInputs(int &scrubbed, int player) {
   string input;
   cout << "Player " << player
@@ -208,7 +294,13 @@ void getInputs(int &scrubbed, int player) {
   scrubbed = inputScrubbing(input);
 }
 
-// initialize each element in the 3D array to contain a 2D array
+/*
+ *Function: initRack
+ * Description: Initialize each element in the 3D array to contain a 2D array
+ * Parameters: N/A
+ * Pre-conditions: User must have initilized a triple pointer
+ * Post Conditions: assigns a 2D array for each player
+ */
 char **initRack() {
   char **rack = new char *[MAX_SIZE];
   for (int i = 0; i < MAX_SIZE; ++i)
@@ -216,28 +308,43 @@ char **initRack() {
   return rack;
 }
 
-// gets total number of players playing the game
+/*
+ *Function: getPlayers
+ * Description: Gets total number of players playing the game
+ * Parameters: int n for total players
+ * Pre-conditions: Program must have started
+ * Post Conditions: assigns total players to n
+ */
 void getPlayers(int &n) {
   bool valid = false;
 
   while (!valid) {
     cout << "How many players?: ";
     cin >> n;
-
-    if (cin.fail()) {
-      cout << "Enter a positive integer" << endl;
-      clearBuffer();
-    } else {
+    if (n >= 1 && n <= 5) {
       clearBuffer();
       valid = true;
+    } else if (n <= 1 || n >= 5) {
+      cout << "Enter a valid integer 1-5 (inclusive)" << endl;
+      clearBuffer();
+    } else if (cin.fail()) {
+      cout << "Enter a positive integer" << endl;
+      clearBuffer();
     }
   }
 }
 
-// this function runs the entire game once for each player
+/*
+ *Function: playGame
+ * Description: Drives entire game for every player
+ * Parameters: triple char pointer, total playes,array for scores
+ * Pre-conditions: User must have typed in valid integer
+ * Post Conditions: Program runs entire game
+ */
 void playGame(char ***racks, int players, int *playerScores) {
 
   int i;
+
   for (i = 0; i < players; i++) {
     racks[i] = initRack();
     int scrubbedInput;
@@ -245,14 +352,24 @@ void playGame(char ***racks, int players, int *playerScores) {
     if (scrubbedInput == -1) {
       // Sets the iterator back by 1 due to invalid input
       errorMsg();
+      for (int j = 0; j < MAX_SIZE; ++j)
+        delete[] racks[i][j];
+      delete[] racks[i];
       --i;
-    } else
+    } else {
       // play a turn for each player
+      playerScores[i] = 0; // Initialize score to 0
       playTurn(racks[i], i + 1, scrubbedInput, playerScores[i]);
+    }
   }
 }
-
-// Game Driver
+/*
+ *Function: main
+ * Description: Drives every function
+ * Parameters: N/A
+ * Pre-conditions: Program must start
+ * Post Conditions: Calls every function in the game
+ */
 int main() {
   srand(time(NULL));
   bool running = true;
